80dc677c1c9a58811715b5a26633dcd5539d5552
Harshrajsinh Raulji <h.raulji2005@gmail.com>
2026-03-19 18:15:09 +0530

    Added API key validation logic in filter with proper request blocking

--- src/main/java/com/harsh/ratelimiter/filter/RateLimitingFilter.java
+++ src/main/java/com/harsh/ratelimiter/filter/RateLimitingFilter.java
@@ -14,7 +14,30 @@ public class RateLimitingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Request intercepted: " + request.getRequestURI());
-        filterChain.doFilter(request,response);
+
+        // Extract API key from request header, This identifies the client making the request
+        String apiKey = request.getHeader("X-API-KEY");
+
+        // Check if API key is missing or empty
+        // If true, we cannot track or limit this user
+        if (apiKey == null || apiKey.isEmpty()) {
+
+            // Set HTTP status to 400 (Bad Request)
+            // This tells client that request is invalid
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+            // Write error message in response body
+            response.getWriter().write("Missing API Key");
+
+            // VERY IMPORTANT:
+            // Stop further execution so request does NOT reach controller
+            return;
+        }
+
+        // If API key is valid → allow request to proceed
+        // This passes request to next filter / controller
+        filterChain.doFilter(request, response);
     }
+
+
 }
